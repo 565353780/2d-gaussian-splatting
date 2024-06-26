e792ed3b63050d27b70ac09731b61379fed19ab0
chLi <565353780@qq.com>
2024-06-26 21:34:58 +0800

    update chmod of demo running bash files

--- render.sh
+++ render.sh
@@ -1,6 +1,5 @@
-python view.py \
+python render.py \
 	-s /home/chli/Dataset/NeRF/hotdog_train \
 	--images dense/images \
 	-m ./output/2b2ac66a-9 \
-	--port 6010 \
-	--iteration 7000
+	--iteration 30000
