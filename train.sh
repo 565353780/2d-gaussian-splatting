11e4fd7b67a41e89fa3ef8853c18aa4ec2e32b3c
chLi <565353780@qq.com>
2025-12-16 20:20:16 +0800

    fix render and eval bugs

--- train.sh
+++ train.sh
@@ -2,4 +2,6 @@ python train.py \
   -s /home/lichanghao/chLi/Dataset/GS/haizei_1/ \
   -m /home/lichanghao/chLi/Dataset/GS/haizei_1/fitting_2dgs/ \
   --images dense/images \
-  --port 6009
+  --port 6009 \
+  --test_iterations 3000000 \
+  --save_iterations 7000 30000
